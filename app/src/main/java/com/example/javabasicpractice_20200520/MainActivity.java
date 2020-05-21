d1e1fda70e302b7996d0300ed8e08ec0f88a35b6
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 10:24:23 +0900

    입력된 아이디/패스워드 확인해서 분기처리  -토스트로 결과 알려줌

--- app/src/main/java/com/example/javabasicpractice_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasicpractice_20200520/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.javabasicpractice_20200520.databinding.ActivityMainBinding;
 
@@ -16,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputId = binding.idEdt.getText().toString();
+                String inputPw = binding.pwEdt.getText().toString();
+
+                Log.d("입력아이디", inputId);
+                Log.d("입력비번", inputPw);
+
+//                안드로이드 sdk에서의 String 비교는 == 이 동작하지 않는다.
+//                이클립스에서는 가능했지만 여기선 안된다.
+//                .equals 로 비교하자..
+                if (inputId.equals("admin") && inputPw.equals("qwer")) {
+//                    Log.d("아이디확인","관리자입니다.");
+                    Toast.makeText(MainActivity.this, "관리자입니다.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+//                    Log.d("아이디확인","관리자가아닙니다.");
+                    Toast.makeText(MainActivity.this, "로그인의 실패했습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
