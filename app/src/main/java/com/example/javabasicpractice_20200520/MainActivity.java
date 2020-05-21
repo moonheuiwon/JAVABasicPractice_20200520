724d360be3f55910c3bd0cd7f4286d7533162ce3
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 10:12:31 +0900

    메인 엑티비티에 데이터바인딩 동작

--- app/src/main/java/com/example/javabasicpractice_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasicpractice_20200520/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.javabasicpractice_20200520;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.javabasicpractice_20200520.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
